e950829e04cbfad5d975db7cb74cd14be7bfeb92
koraytugay <koray@tugay.biz>
2019-08-05 17:01:31 -0400

    GitlabFetchService - shorter

--- src/main/java/biz/tugay/glw/gitlab/service/GitlabFetchService.java
+++ src/main/java/biz/tugay/glw/gitlab/service/GitlabFetchService.java
@@ -2,42 +2,35 @@ package biz.tugay.glw.gitlab.service;
 
 import biz.tugay.glw.gitlab.EmbedInfo;
 import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Scanner;
 
 @Service
+@AllArgsConstructor
 public class GitlabFetchService {
 
     GitlabGateway gitlabGateway;
     GitlabEmbedProcessor gitlabEmbedProcessor;
 
-    public GitlabFetchService(GitlabGateway gitlabGateway, GitlabEmbedProcessor gitlabEmbedProcessor) {
-        this.gitlabGateway = gitlabGateway;
-        this.gitlabEmbedProcessor = gitlabEmbedProcessor;
-    }
-
     @Cacheable(value = "articleCache")
     public String fetchArticle(String projectId, String branch, String path) {
-        String article = gitlabGateway.fetchRawContent(projectId, branch, path);
-
-        String resolvedArticle = "";
+        StringBuilder sb = new StringBuilder();
 
         boolean isContent = true;
-        try (Scanner scanner = new Scanner(article).useDelimiter("-embed-")) {
+        try (Scanner scanner = new Scanner(gitlabGateway.fetchRawContent(projectId, branch, path)).useDelimiter("-embed-")) {
             while (scanner.hasNext()) {
-                if (isContent) {
-                    resolvedArticle = resolvedArticle.concat(scanner.next());
-                } else {
-                    EmbedInfo embedInfo = new Gson().fromJson(scanner.next(), EmbedInfo.class);
-                    resolvedArticle = resolvedArticle.concat(gitlabEmbedProcessor.process(embedInfo));
-                }
+                if (isContent)
+                    sb.append(scanner.next());
+                else
+                    sb.append(gitlabEmbedProcessor.process(new Gson().fromJson(scanner.next(), EmbedInfo.class)));
+
                 isContent = !isContent;
             }
         }
 
-        return resolvedArticle;
+        return sb.toString();
     }
-
 }
