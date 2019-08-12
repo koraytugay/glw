9a46606bf3ae3f629c4fd1481e82d4c7affbc48c
koraytugay <koray@tugay.biz>
2019-08-12 16:28:43 -0400

    Removed pageclap, back and toc back to top

--- src/main/resources/static/js/toc.js
+++ src/main/resources/static/js/toc.js
@@ -1,10 +1,7 @@
 function buildToc() {
-    var toc = '<span class="toc" style="padding-left: 4px"><h4 id="toc-header">Table of Contents</h4><ul>';
+    var toc = '<ul>';
     var inInner = false;
-    var atLeastOneItemFound = false;
     $("h2, h3").each(function () {
-        atLeastOneItemFound = true;
-
         if (this.tagName === 'H3' && !inInner) {
             inInner = true;
             toc = toc.concat('<ul>');
@@ -18,12 +15,9 @@ function buildToc() {
         $(this).attr('id', id);
         toc = toc.concat('<li>').concat('<a href="#' + id + '">').concat(text).concat('</a>').concat('</li>');
     });
-    toc = toc.concat('</ul></span>');
-    if (atLeastOneItemFound) {
-        $("h1").after(toc);
-        $(document.body).addClass('body-shifted');
-
-    }
+    toc = toc.concat('</ul>');
+    toc = toc.concat('</ul>');
+    $("h1").after(toc);
 }
 
 $(document).ready(buildToc);
