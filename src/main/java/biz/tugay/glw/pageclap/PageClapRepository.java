48eb9ea733eeeb46ca406e62651a45ce4206c2bc
koraytugay <koray@tugay.biz>
2019-08-13 14:57:36 -0400

    Got rid of a lot of stuff.

--- src/main/java/biz/tugay/glw/pageclap/PageClapRepository.java
+++
@@ -1,7 +0,0 @@
-package biz.tugay.glw.pageclap;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PageClapRepository extends JpaRepository<PageClap, String> {
-    PageClap findByPath(String path);
-}
