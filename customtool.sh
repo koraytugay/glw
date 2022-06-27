932b0c6fab4effeda5d47b9326ab0258aebee617
Koray Tugay <koraytugay@icloud.com>
2022-06-27 14:17:53 -0400

    Update customtool.sh

--- customtool.sh
+++ customtool.sh
@@ -1,1 +1,8 @@
-
+ function run() {
+       echo "[{ \"type\": \"Global Info\", \
+               \"message\": \"Lift is analyzing commit $commit\", \
+               \"file\": \"file.txt\", \
+               \"category\": \"Info\", \
+               \"details_url\": \"https://example.com/#example\" \
+             }]"
+   }
