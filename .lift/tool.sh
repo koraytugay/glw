2683928faee59a52834bcc01cb3b84396b783b87
Koray Tugay <koraytugay@icloud.com>
2022-06-27 13:58:23 -0400

    Delete tool.sh

--- .lift/tool.sh
+++
@@ -1,8 +0,0 @@
-   function run() {
-       echo "[{ \"type\": \"Global Info\", \
-               \"message\": \"Lift is analyzing commit $commit\", \
-               \"file\": \"file.txt\", \
-               \"category\": \"Info\", \
-               \"details_url\": \"https://example.com/#example\" \
-             }]"
-   }
