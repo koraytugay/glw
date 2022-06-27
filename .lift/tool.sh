76486b001b7fb544dc53bc83419f604d149a9a77
Koray Tugay <koraytugay@icloud.com>
2022-06-27 13:23:36 -0400

    Update tool.sh

--- .lift/tool.sh
+++ .lift/tool.sh
@@ -1,1 +1,2 @@
-dfsafdsafdasfdsafdsa
+function run() { echo "[{ \"type\": \"A but without a line number\", \ \"message\": \"Lift is analyzing commit $commit\", \ \"file\": \"file.txt\", \ \"category\": \"info\", \ \"details_url\": \"https://example.com/#example\" \ }]" }
+
