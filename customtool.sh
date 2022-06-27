1f51cce1224bbdee4b7274cfe24e7d0d0caa8b83
Koray Tugay <koraytugay@icloud.com>
2022-06-27 14:49:01 -0400

    Update customtool.sh

--- customtool.sh
+++ customtool.sh
@@ -1,8 +1,31 @@
- function run() {
-       echo "[{ \"type\": \"Global Info\", \
-               \"message\": \"Lift is analyzing commit $commit\", \
-               \"file\": \"file.txt\", \
-               \"category\": \"Info\", \
-               \"details_url\": \"https://example.com/#example\" \
-             }]"
-   }
+#!/usr/bin/env bash
+dir=$1
+commit=$2
+cmd=$3 
+
+function version() {
+    echo 1
+}
+
+function applicable() {
+    echo "true"
+}
+
+function run() {
+    echo "[{ \"type\": \"Global Info\", \
+            \"message\": \"Lift is analyzing commit $commit\", \
+            \"file\": \"file.txt\", \
+            \"category\": \"Info\", \
+            \"details_url\": \"https://example.com/#example\" \
+          }]"
+}
+
+if [[ "$cmd" = "run" ]] ; then 
+    run 
+fi 
+if [[ "$cmd" = "applicable" ]] ; then 
+    applicable 
+fi 
+if [[ "$cmd" = "version" ]] ; then 
+    version 
+fi
